728c7a99ec30f9f3058299f0c1e24fd53905d365
shubhamjirge <141141304+shubhamjirge@users.noreply.github.com>
2024-01-29 08:51:53 +0530

    Add files via upload

---
+++ PythonDivision.py
@@ -0,0 +1,10 @@
+"""
+Title     : Python: Division
+Subdomain : Introduction
+Domain    : Python
+"""
+if __name__ == "__main__":
+    a = int(input())
+    b = int(input())
+    print(a // b)
+    print(a / b)
